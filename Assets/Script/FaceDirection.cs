3c1615f1bd0ff8a22cfb6deb164233130254361c
hecomi <info@hecomi.com>
2013-08-08 00:52:21 +0900

    カメラの方を向くようにする ＆ 角度制限を追加

--- Assets/Script/FaceDirection.cs
+++ Assets/Script/FaceDirection.cs
@@ -3,23 +3,47 @@ using System.Collections;
 
 public class FaceDirection : MonoBehaviour {
 
-	private GameObject head_   = null;
-	public string headTagName  = "MikuHead";
-	public Camera targetCamera = null;
+	public GameObject Head   = null;
+	public GameObject Target = null;
 	
-	public Vector3 rotationOffset = Vector3.zero;
+	public Vector3 RotationOffset = Vector3.zero;
+	public GameObject SyncHead    = null;
 
-	void Awake () {
-		head_ = GameObject.FindWithTag(headTagName).gameObject;
-		Debug.Log (head_);
-	}
-	
 	void Update () {
-		if (head_ == null || targetCamera == null) return;
-		head_.transform.Rotate(-rotationOffset);
-		Quaternion headRotFrom = head_.transform.rotation;
-		Quaternion headRotTo   = Quaternion.LookRotation(head_.transform.position - targetCamera.transform.position);
-		head_.transform.rotation = Quaternion.Slerp(headRotFrom, headRotTo, 0.05f);
-		head_.transform.Rotate(rotationOffset);
+		if (Head == null || Target == null) return;
+		
+		if (SyncHead != null) {
+			Head.transform.localRotation = SyncHead.transform.localRotation;
+		} else {
+			Head.transform.Rotate(-RotationOffset);
+			Quaternion headRotFrom = Head.transform.rotation;
+			Quaternion headRotTo   = Quaternion.LookRotation(Head.transform.position - Target.transform.position);
+			Head.transform.rotation = Quaternion.Slerp(headRotFrom, headRotTo, 0.05f);
+			Head.transform.Rotate(RotationOffset);
+			
+			// head angle limitation
+			Vector3 localRot = Head.transform.localRotation.eulerAngles;
+			float rotX = (localRot.x + 360) % 360;
+			float rotY = (localRot.y + 360) % 360;
+			float rotZ = (localRot.z + 360) % 360;
+			const float rotLimit = 45;
+			if (rotX > rotLimit && rotX <= 180) {
+				rotX = rotLimit;
+			} else if (rotX >= 180 && rotX < 360 - rotLimit) {
+				rotX = 360 - rotLimit;
+			}
+			if (rotY > rotLimit && rotY <= 180) {
+				rotY = rotLimit;
+			} else if (rotY >= 180 && rotY < 360 - rotLimit) {
+				rotY = 360 - rotLimit;
+			}
+			if (rotZ > rotLimit && rotZ <= 180) {
+				rotZ = rotLimit;
+			} else if (rotZ >= 180 && rotZ < 360 - rotLimit) {
+				rotZ = 360 - rotLimit;
+			}
+			Head.transform.localEulerAngles = new Vector3(rotX, rotY, rotZ);
+		}
+		
 	}
 }
