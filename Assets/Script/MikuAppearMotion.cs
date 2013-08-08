3097917c968fa5c19e79e975e6fa8711231e212d
hecomi <info@hecomi.com>
2013-08-09 01:39:23 +0900

    画面から出てくるモーションを修正

--- Assets/Script/MikuAppearMotion.cs
+++ Assets/Script/MikuAppearMotion.cs
@@ -2,26 +2,42 @@ using UnityEngine;
 using System.Collections;
 
 public class MikuAppearMotion : MonoBehaviour {
+	public GameObject AnotherMiku = null;
+	
 	public Vector3 AppearPositionFrom = Vector3.zero;
 	public Vector3 AppearPositionTo   = Vector3.zero;
 	public Vector3 AppearScaleFrom    = Vector3.zero;
 	public Vector3 AppearScaleTo      = Vector3.zero;
+	
 	public float   Velocity   = 0.02f;
 	public float   Diff       = 30.0f;
+	
 	private bool endFlag_     = false;
+	private bool startFlag_   = false;
+	
+	public void Appear() {
+		if (startFlag_ == true) return;
+		startFlag_ = true;
+		
+		// appear motion start
+		transform.animation.CrossFade("Appear");
+		AnotherMiku.GetComponent<MikuAppearMotion>().Appear();
+	}
 	
 	void Awake() {
+		if (AnotherMiku == null) {
+			Debug.LogError("Another Miku is not set!");	
+		}
+		
 		AppearPositionTo += transform.localPosition;
 		transform.localPosition += AppearPositionFrom;
 		
 		AppearScaleTo += transform.localScale;
 		transform.localScale += AppearScaleFrom;
-		
-		animation.CrossFade("Appear");
 	}
 	
 	void Update() {
-		if (endFlag_ == true) return;
+		if (startFlag_ == false || endFlag_ == true) return;
 		
 		transform.localPosition = Vector3.Slerp(transform.localPosition, AppearPositionTo, Velocity);
 		transform.localScale    = Vector3.Slerp(transform.localScale,    AppearScaleTo,    Velocity);
